da88445bc08bc24ca77c4189b9b2fa2ad5178714
lilleplutt <alin.alin.alin2000@yandex.ru>
2025-06-10 10:35:31 +0300

    добавлены кнопки да и нет

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,3 +1,10 @@
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+         static var yPGreen: UIColor { UIColor(named: "YP Green") ?? UIColor.green }
+         static var yPRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
+         static var yPBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black}
+         static var yPBackground: UIColor { UIColor(named: "YP Background") ?? UIColor.darkGray }
+         static var yPGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
+         static var yPWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white}
+}
