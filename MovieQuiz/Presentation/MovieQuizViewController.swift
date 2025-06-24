5c42b50651c4aabbe4541135c8c85dca7c34867c
lilleplutt <alin.alin.alin2000@yandex.ru>
2025-06-24 12:33:11 +0300

    добавлена главная очередь

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -47,7 +47,10 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
         
             currentQuestion = question
             let viewModel = convert(model: question)
-            show(quiz: viewModel)
+        
+        DispatchQueue.main.async { [weak self] in
+            self?.show(quiz: viewModel)
+        }
     }
     
     //MARK: - Private functions
