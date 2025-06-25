6a27fa99de9481c153ee3aaeb53444d42538e2c2
lilleplutt <alin.alin.alin2000@yandex.ru>
2025-06-25 10:59:03 +0300

    добавлено замыкание в структуру и ссылка на делегат и его инициализатор в класс

--- MovieQuiz/Models/AlertModel.swift
+++ MovieQuiz/Models/AlertModel.swift
@@ -4,7 +4,6 @@ struct AlertModel {
     let title: String
     let message: String
     let buttonText: String
-    
-    
+    let completion: (() -> Void)?
 }
 
--- MovieQuiz/Presentation/AlertPresenter.swift
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -2,6 +2,10 @@ import Foundation
 import UIKit
 
 class AlertPresenter {
+    weak var viewController: UIViewController?
     
+    init(viewController: UIViewController?) {
+        self.viewController = viewController
+    }
 }
 
