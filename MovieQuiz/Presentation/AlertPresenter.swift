6d7313b9eb3a2cd0b6eceec8ee0b4adb3d2ad0dc
lilleplutt <alin.alin.alin2000@yandex.ru>
2025-06-25 11:29:47 +0300

    добавлен метод показа алерта в класс алерта

--- MovieQuiz/Presentation/AlertPresenter.swift
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -2,10 +2,25 @@ import Foundation
 import UIKit
 
 class AlertPresenter {
-    weak var viewController: UIViewController?
+    private weak var viewController: UIViewController?
     
     init(viewController: UIViewController?) {
         self.viewController = viewController
+    }
+    
+    func show(alert model: AlertModel) {
+        let alert = UIAlertController(
+            title: model.title,
+            message: model.message,
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: model.buttonText, style: .default) { _ in
+            model.completion?()
+        }
+        
+        alert.addAction(action)
+        viewController?.present(alert, animated: true)
+        
     }
 }
 
