7e23b4882b3e402d571da5fa4aeb3c88d82a1bc7
lilleplutt <alin.alin.alin2000@yandex.ru>
2025-06-30 10:25:22 +0300

    заполнен геттер и сеттер для кол-ва игр

--- MovieQuiz/Presentation/StaticticService.swift
+++ MovieQuiz/Presentation/StaticticService.swift
@@ -1,9 +1,30 @@
 import Foundation
 
 final class StaticticService: StatisticServiceProtocol {
-    var gamesCount: Int
-    var bestGame: GameResult
-    var totalAccuracy: Double 
+    var gamesCount: Int {
+        get {
+            UserDefaults.standard.integer(forKey: "gamesCount")
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: "gamesCount")
+        }
+    }
+    var bestGame: GameResult {
+        get {
+            
+        }
+        set {
+            
+        }
+    }
+    var totalAccuracy: Double {
+        get {
+            
+        }
+        set {
+            
+        }
+    }
     
     func store(correct count: Int, total amount: Int) {
         <#code#>
