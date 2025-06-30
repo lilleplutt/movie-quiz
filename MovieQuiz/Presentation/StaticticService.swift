eceb5727166905f7f5f415490e66f80a0f9bffcc
lilleplutt <alin.alin.alin2000@yandex.ru>
2025-06-30 11:05:07 +0300

    заполнен геттер и сеттер для лучшей игры

--- MovieQuiz/Presentation/StaticticService.swift
+++ MovieQuiz/Presentation/StaticticService.swift
@@ -3,7 +3,7 @@ import Foundation
 final class StaticticService: StatisticServiceProtocol {
     var gamesCount: Int {
         get {
-            UserDefaults.standard.integer(forKey: "gamesCount")
+             UserDefaults.standard.integer(forKey: "gamesCount")
         }
         set {
             UserDefaults.standard.set(newValue, forKey: "gamesCount")
@@ -11,10 +11,16 @@ final class StaticticService: StatisticServiceProtocol {
     }
     var bestGame: GameResult {
         get {
+            let correct = UserDefaults.standard.integer(forKey: "bestGameCorrect")
+            let total = UserDefaults.standard.integer(forKey: "bestGameTotal")
+            let date = UserDefaults.standard.object(forKey: "bestGameDate") as? Date ?? Date()
             
+            return GameResult(correct: correct, total: total, date: date)
         }
         set {
-            
+            UserDefaults.standard.set(newValue.correct, forKey: "bestGameCorrect")
+            UserDefaults.standard.set(newValue.total, forKey: "bestGameTotal")
+            UserDefaults.standard.set(newValue.date, forKey: "bestGameDate")
         }
     }
     var totalAccuracy: Double {
