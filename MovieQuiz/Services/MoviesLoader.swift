777dd2e10c9757c0fe776466fe5f963359110a07
lilleplutt <alin.alin.alin2000@yandex.ru>
2025-07-14 11:38:18 +0300

    добавлен url

--- MovieQuiz/Services/MoviesLoader.swift
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -5,7 +5,17 @@ protocol MoviesLoading {
 }
 
 struct MoviesLoader: MoviesLoading {
+    
+    // MARK: - NetworkClient
     private let networkClient = NetworkClient()
+    
+    // MARK: - URL
+    private var mostPopularMoviesUrl: URL {
+        guard let url = URL(string: "https://tv-api.com/en/API/Top250Movies/k_kiwxbi4y") else {
+            preconditionFailure("Unable to construct mostPopularMoviesUrl")
+            }
+        return url
+        }
     
     func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
     
