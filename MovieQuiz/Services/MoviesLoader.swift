f6b9af3bccd5ed9daf8596693d1c436cd4021f80
lilleplutt <alin.alin.alin2000@yandex.ru>
2025-07-14 11:29:04 +0300

    добавлена переменная нетворка в загрузчик фильмов

--- MovieQuiz/Services/MoviesLoader.swift
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -3,3 +3,11 @@ import Foundation
 protocol MoviesLoading {
     func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void)
 }
+
+struct MoviesLoader: MoviesLoading {
+    private let networkClient = NetworkClient()
+    
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
+    
+    }
+}
