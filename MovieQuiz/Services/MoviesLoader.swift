730778f3b5c621c2f7d2628282a87521dfde5f3b
lilleplutt <alin.alin.alin2000@yandex.ru>
2025-07-15 14:26:54 +0300

    добавлен метод загрузки фильмов с сервера

--- MovieQuiz/Services/MoviesLoader.swift
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -13,11 +13,25 @@ struct MoviesLoader: MoviesLoading {
     private var mostPopularMoviesUrl: URL {
         guard let url = URL(string: "https://tv-api.com/en/API/Top250Movies/k_kiwxbi4y") else {
             preconditionFailure("Unable to construct mostPopularMoviesUrl")
-            }
-        return url
         }
+        return url
+    }
     
     func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
-    
+        
+       networkClient.fetch(url: mostPopularMoviesUrl) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let mostPopularMovies = try JSONDecoder().decode(MostPopularMovies.self, from: data)
+                    handler(.success(mostPopularMovies))
+                } catch {
+                    handler(.failure(error))
+                }
+                
+            case .failure(let error):
+                handler(.failure(error))
+            }
+        }
     }
 }
