331bda9e93e878eae45ca8172b253a133a936042
lilleplutt <alin.alin.alin2000@yandex.ru>
2025-07-15 14:54:46 +0300

    тип картинки изменен на data в модели вопроса и методе конвертирования

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -103,7 +103,7 @@ class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, AlertP
     
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
         return QuizStepViewModel(
-            image: UIImage(named: model.image) ?? UIImage(),
+            image: UIImage(data: model.image) ?? UIImage(),
             question: model.text,
             questionNumber: "\(currentQuestionIndex + 1)/\(questionsAmount)")
     }
