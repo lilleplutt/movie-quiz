98517a422dfad6d07fedcd11c2e8b3c9cb93d39e
lilleplutt <alin.alin.alin2000@yandex.ru>
2025-07-23 10:49:08 +0300

    скопирован метод конвертации в презентер

--- MovieQuiz/Presentation/MovieQuizPresenter.swift
+++ MovieQuiz/Presentation/MovieQuizPresenter.swift
@@ -1,2 +1,12 @@
+import UIKit
 
+final class MovieQuizPresenter {
+    
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        return QuizStepViewModel(
+            image: UIImage(data: model.image) ?? UIImage(),
+            question: model.text,
+            questionNumber: "\(currentQuestionIndex + 1)/\(questionsAmount)")
+    }
+}
 
