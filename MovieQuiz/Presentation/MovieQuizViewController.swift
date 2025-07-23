c83e3988b84494cf5e0b2f817bd3bcc92b029daf
lilleplutt <alin.alin.alin2000@yandex.ru>
2025-07-23 10:54:41 +0300

    удален метод конвертации из вьюКонтроллера

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -104,13 +104,6 @@ class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, AlertP
         alertPresenter.show(alert: model)
     }
     
-    private func convert(model: QuizQuestion) -> QuizStepViewModel {
-        return QuizStepViewModel(
-            image: UIImage(data: model.image) ?? UIImage(),
-            question: model.text,
-            questionNumber: "\(currentQuestionIndex + 1)/\(questionsAmount)")
-    }
-    
     private func show(quiz step: QuizStepViewModel) {
         imageView.image = step.image
         counterLabel.text = step.questionNumber
