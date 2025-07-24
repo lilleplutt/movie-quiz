44af3f11f726a4800a64b70aab76c5b6db5a740f
lilleplutt <alin.alin.alin2000@yandex.ru>
2025-07-24 17:15:59 +0300

    добавлена приватность полям презентера

--- MovieQuiz/Presentation/MovieQuizPresenter.swift
+++ MovieQuiz/Presentation/MovieQuizPresenter.swift
@@ -4,11 +4,11 @@ final class MovieQuizPresenter: QuestionFactoryDelegate {
     
     //MARK: - Properties
     
-    let questionsAmount: Int = 10
-    var currentQuestionIndex: Int = 0
-    var currentQuestion: QuizQuestion?
+    private let questionsAmount: Int = 10
+    private var currentQuestionIndex: Int = 0
+    private var currentQuestion: QuizQuestion?
     private weak var viewController: MovieQuizViewController?
-    var correctAnswers: Int = 0
+    private var correctAnswers: Int = 0
     private var questionFactory: QuestionFactoryProtocol?
     private let statisticService: StatisticServiceProtocol!
     
