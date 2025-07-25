5b711e4461b81e4265d0e0e7865caf1e84e745cb
lilleplutt <alin.alin.alin2000@yandex.ru>
2025-07-25 09:57:18 +0300

    добавлен тест презентера

--- MovieQuizTests/PresenterTests.swift
+++ MovieQuizTests/PresenterTests.swift
@@ -28,3 +28,19 @@ final class MovieQuizViewControllerMock: MovieQuizViewControllerProtocol {
     }
 }
 
+final class MovieQuizPresenterTests: XCTestCase {
+    func testPresenterConvertModel() throws {
+        let viewControllerMock = MovieQuizViewControllerMock()
+        let sut = MovieQuizPresenter(viewController: viewControllerMock)
+        
+        let emptyData = Data()
+        let question = QuizQuestion(image: emptyData, text: "Question text", correctAnswer: true)
+        let viewModel = sut.convert(model: question)
+        
+        XCTAssertNotNil(viewModel.image)
+        XCTAssertEqual(viewModel.question, "Question text")
+        XCTAssertEqual(viewModel.questionNumber, "1/10")
+        
+    }
+}
+
