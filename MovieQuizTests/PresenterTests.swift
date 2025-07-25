a2bb438bfffc6c9f1ff02e6dfdff20dbf4c7e3e7
lilleplutt <alin.alin.alin2000@yandex.ru>
2025-07-25 09:50:13 +0300

    создан тест для презентера с моком

---
+++ MovieQuizTests/PresenterTests.swift
@@ -0,0 +1,30 @@
+import Foundation
+import XCTest
+@testable import MovieQuiz
+
+final class MovieQuizViewControllerMock: MovieQuizViewControllerProtocol {
+    func show(quiz step: MovieQuiz.QuizStepViewModel) {
+        
+    }
+    
+    func show(result: MovieQuiz.QuizResultsViewModel) {
+        
+    }
+    
+    func highlightImageBorder(isCorrectAnswer: Bool) {
+        
+    }
+    
+    func showLoadingIndicator() {
+        
+    }
+    
+    func hideLoadingIndicator() {
+        
+    }
+    
+    func showNetworkError(message: String) {
+        
+    }
+}
+
